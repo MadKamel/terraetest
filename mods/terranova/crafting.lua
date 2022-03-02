8c16280d074410097b5eb83a6e3079d1bde4daee
MadKamel <rustrumek@gmail.com>
2022-03-02 10:19:10 -0800

    Added in some crafts.
    This will be how crafts work in this game now.

--- mods/terranova/crafting.lua
+++ mods/terranova/crafting.lua
@@ -1,5 +1,33 @@
-minetest.register_node("terranova:craftbench_r", {
-	description = "Craft Bench Right",
-	paramtype = "light",
-	groups = {building = 1},
+milestones = {}
+terranova = {}
+terranova.register_craft = function(output, data)
+end
+
+
+
+terranova.register_craft("iron_ingot", {
+	input = {"magnetite_lump"},
+	type = {"craftbench", "smelter"},
+	milestone = 0,
+	alternate = false,
+	time = 1,
+	name = "Iron Ingot"
+})
+
+terranova.register_craft("iron_rod 2", {
+	input = {"iron_ingot"},
+	type = {"craftbench", "constructor"},
+	milestone = 0,
+	alternate = false,
+	time = 1,
+	name = "Iron Rod"
+})
+
+terranova.register_craft("iron_plate 3", {
+	input = {"iron_ingot 2"},
+	type = {"craftbench", "constructor"},
+	milestone = 1,
+	alternate = false,
+	time = 1,
+	name = "Iron Plate"
 })
