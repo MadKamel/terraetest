56b7a1c0b5457b60470d81f23cfe50d492c48a4b
MadKamel <rustrumek@gmail.com>
2022-03-02 12:14:14 -0800

    Added in the main function.
    This should be enough for now.

--- mods/terranova/crafting.lua
+++ mods/terranova/crafting.lua
@@ -1,11 +1,25 @@
-milestones = {}
-terranova = {}
-terranova.register_craft = function(output, data)
+function get_table_size(t)
+	local count = 0
+	for _, __ in pairs(t) do
+		count = count + 1
+	end
+	return count
 end
 
+terranova = {}
+terranova.crafts = {}
+terranova.register_craft = function(output, data)
+	terranova.crafts[get_table_size(terranova.crafts)] = data
+end
+
+minetest.register_node("terranova:craft_bench", {
+	description = "Workshop"
+}
 
 
-terranova.register_craft("iron_ingot", {
+
+terranova.register_craft({
+	output = "iron_ingot",
 	input = {"magnetite_lump"},
 	type = {"craftbench", "smelter"},
 	milestone = 0,
@@ -14,7 +28,8 @@ terranova.register_craft("iron_ingot", {
 	name = "Iron Ingot"
 })
 
-terranova.register_craft("iron_rod 2", {
+terranova.register_craft({
+	output = "iron_rod 2",
 	input = {"iron_ingot"},
 	type = {"craftbench", "constructor"},
 	milestone = 0,
@@ -23,7 +38,8 @@ terranova.register_craft("iron_rod 2", {
 	name = "Iron Rod"
 })
 
-terranova.register_craft("iron_plate 3", {
+terranova.register_craft({
+	output = "iron_plate 3",
 	input = {"iron_ingot 2"},
 	type = {"craftbench", "constructor"},
 	milestone = 1,
