d742c5a2060d742901b85a308fbfaccd61597e80
MadKamel <rustrumek@gmail.com>
2022-03-03 08:04:50 -0800

    Update utils.lua

--- mods/terranova/utils.lua
+++ mods/terranova/utils.lua
@@ -1,3 +1,13 @@
 utils = {}
-utils.register_recipe = function()
+utils.function get_table_size(t)
+	local count = 0
+	for _, __ in pairs(t) do
+		count = count + 1
+	end
+	return count
+end
+
+utils.crafts = {}
+utils.register_craft = function(output, data)
+	utils.crafts[get_table_size(terranova.crafts)] = data
 end
