183e99e1230ae869e0d708d404b1db0da67c7160
MadKamel <rustrumek@gmail.com>
2022-03-03 08:55:13 -0800

    Create milestones.lua

---
+++ mods/terranova/milestones.lua
@@ -0,0 +1,9 @@
+-- This file is here to enable the game-progression system I had in mind. Specifically, sinking specific materials into the milestone terminal
+--    will unlock the associated crafting recipes. For example, see below for an example.
+
+--	utils.register_milestone(1, {
+--		cost = {"terranova:iron_rod 20", "terranova:iron_plate 15"},
+--		name = "Basic Iron Production"
+--	})
+
+-- This code snippet will register milestone 1, the first milestone, to have a cost (what you need to sink) and a name (the display title).
