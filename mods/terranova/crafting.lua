e53cd90765e2ff05a801e1bbfed12dabd99005f3
MadKamel <rustrumek@gmail.com>
2022-03-03 08:04:02 -0800

    Update crafting.lua

--- mods/terranova/crafting.lua
+++ mods/terranova/crafting.lua
@@ -1,17 +1,3 @@
-function get_table_size(t)
-	local count = 0
-	for _, __ in pairs(t) do
-		count = count + 1
-	end
-	return count
-end
-
-terranova = {}
-terranova.crafts = {}
-terranova.register_craft = function(output, data)
-	terranova.crafts[get_table_size(terranova.crafts)] = data
-end
-
 minetest.register_node("terranova:craft_bench", {
 	description = "Workshop"
 }
@@ -42,7 +28,7 @@ terranova.register_craft({
 	output = "iron_plate 3",
 	input = {"iron_ingot 2"},
 	type = {"craftbench", "constructor"},
-	milestone = 1,
+	milestone = 0,
 	alternate = false,
 	time = 1,
 	name = "Iron Plate"
