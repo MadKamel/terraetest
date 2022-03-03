37ded7753561968dcfb7634233a7529238821b44
MadKamel <rustrumek@gmail.com>
2022-03-03 09:47:53 -0800

    Added first milestone.

--- mods/terranova/milestones.lua
+++ mods/terranova/milestones.lua
@@ -7,3 +7,10 @@
 --	})
 
 -- This code snippet will register milestone 1, the first milestone, to have a cost (what you need to sink) and a name (the display title).
+
+
+
+utils.register_milestone(1, {
+	cost = {"terranova:iron_rod 20", "terranova:iron_plate 15"},
+	name = "Basic Iron Production"
+})
