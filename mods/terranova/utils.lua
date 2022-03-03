a505bff5f29575d84a363b707518467b02dc3e86
MadKamel <rustrumek@gmail.com>
2022-03-03 10:06:29 -0800

    Added in player_has_items
    It's a work in progress. I still need to work on how I'll get this to work.

--- mods/terranova/utils.lua
+++ mods/terranova/utils.lua
@@ -1,5 +1,5 @@
 utils = {}
-utils.function get_table_size(t)
+utils.get_table_size = function(t)
 	local count = 0
 	for _, __ in pairs(t) do
 		count = count + 1
@@ -7,7 +7,20 @@ utils.function get_table_size(t)
 	return count
 end
 
+
 utils.crafts = {}
+utils.milestones = {}
+
+utils.player_has_items = function(items_requested, player)
+	local inv = player:get_inventory()
+	local items_in_inventory = {}
+	minetest.log(dump(inv)) --TODO: Get each itemstack in player inventory, combine the amounts for each unique item and store it in items_in_inventory
+end
+
 utils.register_craft = function(output, data)
 	utils.crafts[get_table_size(terranova.crafts)] = data
+end
+
+utils.register_milestone = function(number, data)
+	utils.milestone[number] = data
 end
